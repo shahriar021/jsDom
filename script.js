dd13072f33119763a8f2c059c67b8209112d815a
shahriar021 <shahriarc19@gmail.com>
2023-08-03 02:22:11 +0600

    first

---
+++ script.js
@@ -0,0 +1,58 @@
+// 4 pillar of dom
+
+// 1.selection of an Element
+// let a =document.querySelector("h1")
+// console.log(a)
+//---------------------------------------------
+
+// 2.changing HTML
+// let a = document.querySelector("h1");
+// a.innerHTML = "Hello World";
+//---------------------------------------------
+
+// 3.changing Style
+// let a = document.querySelector("h1");
+// a.style.color = "red";
+
+// a.style.backgroudColor = "black";
+//---------------------------------------------
+// 4.event listener
+// let a = document.querySelector("h1");
+// a.style.backgroundColor="red"
+// a.addEventListener("click",function(){
+//     alert("I got clicked")
+//     a.innerHTML="now i'm changed"
+//     a.style.color="white"
+// })
+
+//bulb program-----------------------------------
+// let bulb = document.querySelector("#bulb")
+// let btnOn = document.querySelector("#on")
+// let btnOff = document.querySelector("#off")
+
+// // btnOn.addEventListener("click",function(){
+// //     bulb.style.backgroundColor="yellow"
+// // })
+
+// // btnOff.addEventListener("click", function () {
+// //   bulb.style.backgroundColor = "black";
+// // });
+
+// let isOn = false;
+
+// btnOn.addEventListener("click", function () {
+//     if(isOn==0){
+//         bulb.style.backgroundColor = "yellow";
+//         isOn = true;
+//     }
+//     else{
+//         bulb.style.backgroundColor = "transparent";
+//         isOn = false;
+//     }
+  
+// });
+
+document.querySelectorAll("#on"); 
+
+
+
